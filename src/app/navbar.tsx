200abd8fc12913387101a37823201e74f42558af
ilyasofficial1617 <ilyasofficial1617@gmail.com>
2023-06-18 15:56:10 +0700

    navbar navigation with active link

--- src/app/navbar.tsx
+++ src/app/navbar.tsx
@@ -1,16 +1,34 @@
+"use client";
+
+import Link from "next/link";
+import { usePathname } from "next/navigation";
+
+const menu = [
+  { title: "WebDev", path: "/webdev" },
+  { title: "AI / ML", path: "/aiml" },
+  { title: "DevOps", path: "/devops" },
+  { title: "Project", path: "/project" },
+  { title: "Personal", path: "/personal" },
+];
+
 export default function NavBar() {
+  const pathname = usePathname();
   return (
-    <div className="flex h-12 items-center justify-between bg-teal-400">
-      <div className="px-2 text-2xl font-semibold text-teal-50">
-        ilyasofficial1617
-      </div>
-      <div className="flex flex-row text-teal-50">
-        <div>Web Dev</div>
-        <div>AI / ML</div>
-        <div>Dev Ops</div>
-        <div>Projects</div>
-        <div>Personal</div>
-      </div>
+    <div className="flex h-12 items-center justify-between bg-teal-400 px-2 text-teal-50">
+      <div className="px-2 text-2xl font-bold">ilyasofficial1617</div>
+      <ul className="flex flex-row font-medium">
+        {menu.map(function (item, index) {
+          const isActive = pathname.startsWith(item.path);
+          const addClass = isActive ? " text-amber-200" : "";
+          return (
+            <li key={index}>
+              <Link href={item.path} className={"m-2 p-2" + addClass}>
+                {item.title}
+              </Link>
+            </li>
+          );
+        })}
+      </ul>
     </div>
   );
 }
